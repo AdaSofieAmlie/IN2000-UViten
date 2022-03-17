c71ec873436f82cfd3f88a06b17f899ee1b67e1a
ulrikgb2000 <ulrikbrunner71@gmail.com>
2022-03-17 09:42:19 +0100

    Implementation 1 - MapBox, basic map

--- app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
+++ app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
@@ -7,16 +7,20 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.appen.databinding.FragmentDashboardBinding
+import com.example.appen.databinding.FragmentMapBinding
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
 
 class MapFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentMapBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    //Mapbox
+    var mapView: MapView? = null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,19 +29,38 @@ class MapFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        //
+        mapView = binding.mapView
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        mapView?.onDestroy()
     }
 
     // TestKOmmentar
