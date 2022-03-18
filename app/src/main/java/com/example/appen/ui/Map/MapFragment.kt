ec58cfae3011048a087c1743e588e69c5e5b85a4
ulrikgb2000 <ulrikbrunner71@gmail.com>
2022-03-18 10:23:09 +0100

    Implementation 2 - Many errors, need fix

--- app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
+++ app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
@@ -1,18 +1,37 @@
 package com.example.appen.ui.Map
 
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.appen.R
 import com.example.appen.databinding.FragmentMapBinding
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import java.lang.ref.WeakReference
 
 
-class MapFragment : Fragment() {
+///
+
+
+class MapFragment : Fragment(){ //OnMapReadyCallback
 
     private var _binding: FragmentMapBinding? = null
 
@@ -20,12 +39,46 @@ class MapFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     //Mapbox
-    var mapView: MapView? = null
+
+        //User location
+    private lateinit var mapView: MapView
+
+
+    lateinit var permissionsManager: PermissionsManager
+
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
+        mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
+    /*
+    var map: MapboxMap? = null
+    var permissionsManager: PermissionsManager? = null
+    var locationEngine: LocationEngine? = null
+    var locationLayerPlugin: LocationLayer? = null
+    var originLocation: Location? = null
+    */
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         val dashboardViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
 
@@ -39,8 +92,83 @@ class MapFragment : Fragment() {
         //
         mapView = binding.mapView
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+        //Test med premissions
+        setContentView(mapView)
+        permissionsManager = PermissionsManager(WeakReference(this)) //Premission Listner
+        permissionsManager.checkPermissions {
+            onMapReady()
+        }
+
+
         return root
     }
+    private fun onMapReady() {
+        mapView.getMapboxMap().setCamera(
+            CameraOptions.Builder()
+                .zoom(14.0)
+                .build()
+        )
+        mapView.getMapboxMap().loadStyleUri(
+            Style.MAPBOX_STREETS
+        ) {
+            initLocationComponent()
+            setupGesturesListener()
+        }
+    }
+    private fun setupGesturesListener() {
+        mapView.gestures.addOnMoveListener(onMoveListener)
+    }
+    private fun initLocationComponent() {
+        val locationComponentPlugin = mapView.location
+        locationComponentPlugin.updateSettings {
+            this.enabled = true
+            LocationPuck2D(
+                bearingImage = AppCompatResources.getDrawable(
+                    this@MapFragment,
+                    R.drawable.mapbox_user_puck_icon,
+                ),
+                shadowImage = AppCompatResources.getDrawable(
+                    this@MapFragment,
+                    R.drawable.mapbox_user_icon_shadow,
+                ),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.6)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(1.0)
+                    }
+                }.toJson()
+            ).also { this.locationPuck = it }
+        }
+        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+    }
+
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissions.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
 
     override fun onStart() {
         super.onStart()
@@ -61,7 +189,12 @@ class MapFragment : Fragment() {
         super.onDestroyView()
         _binding = null
         mapView?.onDestroy()
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+
     }
 
-    // TestKOmmentar
 }
