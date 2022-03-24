dd0020f5f3dbaaa412afbcbec6af2bd6979ff646
buljong1 <hermanvest@gmail.com>
2022-03-24 09:00:03 +0100

    Prøver å committe swipe-funksjon igjen

--- app/src/main/java/com/example/appen/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/example/appen/ui/Home/HomeFragment.kt
@@ -4,40 +4,66 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.appen.databinding.FragmentHomeBinding
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.appen.R
 
 class HomeFragment : Fragment() {
+    // When requested, this adapter returns a DemoObjectFragment,
+    // representing an object in the collection.
+    private lateinit var demoCollectionAdapter: HomeCollectionAdapter
+    private lateinit var viewPager: ViewPager2
 
-    private var _binding: FragmentHomeBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        demoCollectionAdapter = HomeCollectionAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = demoCollectionAdapter
+    }
+}
+
+class HomeCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        // Return a NEW fragment instance in createFragment(int)
+        if (position==0) return SimpleDisplayFragment()
+        else return AdvancedDisplayFragment()
+    }
+}
+
+private const val ARG_OBJECT = "object"
+
+// Instances of this class are fragments representing a single
+// object in our collection.
+class SimpleDisplayFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return inflater.inflate(R.layout.fragment_simple_display, container, false)
     }
+}
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+class AdvancedDisplayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_advanced_display, container, false)
     }
 }
 
