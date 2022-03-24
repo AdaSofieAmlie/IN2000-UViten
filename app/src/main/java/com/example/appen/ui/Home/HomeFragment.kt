d8b5fa9e379659ddec50ca1fa4cd55b3bdb22ed6
buljong1 <hermanvest@gmail.com>
2022-03-24 08:38:47 +0100

    Revert "Lagt til swipe funksjon"
    This reverts commit abcedf40

--- app/src/main/java/com/example/appen/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/example/appen/ui/Home/HomeFragment.kt
@@ -7,81 +7,37 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.example.appen.R
 import com.example.appen.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    // When requested, this adapter returns a DemoObjectFragment,
-    // representing an object in the collection.
-    private lateinit var demoCollectionAdapter: DemoCollectionAdapter
-    private lateinit var viewPager: ViewPager2
+    private var _binding: FragmentHomeBinding? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        demoCollectionAdapter = DemoCollectionAdapter(this)
-        viewPager = view.findViewById(R.id.pager)
-        viewPager.adapter = demoCollectionAdapter
-    }
-}
-
-class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-
-    override fun getItemCount(): Int = 2
-
-    override fun createFragment(position: Int): Fragment {
-        // Return a NEW fragment instance in createFragment(int)
-        if (position == 0) return SimpleDisplayFragment()
-        else return AdvancedDisplayFragment()
-    }
-}
-
-private const val ARG_OBJECT = "object"
-
-// Instances of this class are fragments representing a single
-// object in our collection.
-class SimpleDisplayFragment : Fragment() {
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.simple_display_home, container, false)
-    }
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
-            val textView: TextView = view.findViewById(android.R.id.text1)
-            textView.text = getInt(ARG_OBJECT).toString()
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
         }
-    }
-}
-
-class AdvancedDisplayFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.advanced_display_home, container, false)
+        return root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
-            val textView: TextView = view.findViewById(android.R.id.text1)
-            textView.text = getInt(ARG_OBJECT).toString()
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
 
