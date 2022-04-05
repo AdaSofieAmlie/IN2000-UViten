fc3695cf2352747f645b1bf9ab4d063d8abd0675
Snorrew <snorrw@uio.no>
2022-04-05 20:47:41 +0200

    HeatMap - Earthquake

--- app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
+++ app/src/main/java/com/example/appen/ui/Map/MapFragment.kt
@@ -12,10 +12,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.appen.databinding.FragmentMapBinding
 import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory.*
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 import com.mapbox.maps.plugin.LocationPuck2D
 import com.mapbox.maps.plugin.gestures.OnMoveListener
 import com.mapbox.maps.plugin.gestures.gestures
@@ -23,6 +26,16 @@ import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListene
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
 import java.lang.ref.WeakReference
+import com.mapbox.maps.extension.style.layers.addLayerAbove
+import com.mapbox.maps.extension.style.layers.addLayerBelow
+import com.mapbox.maps.extension.style.layers.generated.CircleLayer
+import com.mapbox.maps.extension.style.layers.generated.HeatmapLayer
+import com.mapbox.maps.extension.style.layers.generated.circleLayer
+import com.mapbox.maps.extension.style.layers.generated.heatmapLayer
+import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
+import com.mapbox.maps.plugin.MapProjection
+
 
 class MapFragment : Fragment(){ //OnMapReadyCallback
 
@@ -31,6 +44,7 @@ class MapFragment : Fragment(){ //OnMapReadyCallback
 
     //Mapbox
         //User location
+    private lateinit var mapboxMap: MapboxMap
     private lateinit var mapView: MapView
     private lateinit var locationPermissionHelper: LocationPermissionHelper
 
@@ -60,6 +74,8 @@ class MapFragment : Fragment(){ //OnMapReadyCallback
         savedInstanceState: Bundle?
     ): View {
 
+
+
         val dashboardViewModel =
             ViewModelProvider(this).get(MapViewModel::class.java)
 
@@ -71,13 +87,19 @@ class MapFragment : Fragment(){ //OnMapReadyCallback
             textView.text = it
         }
 
+
         mapView = binding.mapView
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
 
-        mapView
         locationPermissionHelper = LocationPermissionHelper(WeakReference(activity))
         locationPermissionHelper.checkPermissions {
             onMapReady()
+        }
+        mapboxMap = binding.mapView.getMapboxMap().apply {
+            loadStyleUri(
+                styleUri = Style.LIGHT
+            ) { style -> addRuntimeLayers(style) }
+            setMapProjection(MapProjection.Globe)
         }
         return root
     }
@@ -88,14 +110,13 @@ class MapFragment : Fragment(){ //OnMapReadyCallback
                 .zoom(14.0)
                 .build()
         )
-        mapView.getMapboxMap().loadStyleUri(
-            Style.MAPBOX_STREETS
-        ) {
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) {
             Log.d(null,"Før locationComp og GestureListener")
             initLocationComponent()
             setupGesturesListener()
             Log.d(null,"LocationCompListener og CameraGestureListener kjører")
         }
+
     }
 
     private fun setupGesturesListener() {
@@ -190,6 +211,219 @@ class MapFragment : Fragment(){ //OnMapReadyCallback
         mapView.gestures.removeOnMoveListener(onMoveListener)
         Log.d(null,"... Destroy kart")
 
+    }
+    private fun addRuntimeLayers(style: Style) {
+        style.addSource(createEarthquakeSource())
+        style.addLayerAbove(createHeatmapLayer(), "waterway-label")
+        style.addLayerBelow(createCircleLayer(), HEATMAP_LAYER_ID)
+    }
+
+    private fun createEarthquakeSource(): GeoJsonSource {
+        return geoJsonSource(EARTHQUAKE_SOURCE_ID) {
+            url(EARTHQUAKE_SOURCE_URL)
+        }
+    }
+
+    private fun createHeatmapLayer(): HeatmapLayer {
+        return heatmapLayer(
+            HEATMAP_LAYER_ID,
+            EARTHQUAKE_SOURCE_ID
+        ) {
+            maxZoom(9.0)
+            sourceLayer(HEATMAP_LAYER_SOURCE)
+            // Begin color ramp at 0-stop with a 0-transparancy color
+            // to create a blur-like effect.
+            heatmapColor(
+                interpolate {
+                    linear()
+                    heatmapDensity()
+                    stop {
+                        literal(0)
+                        rgba(33.0, 102.0, 172.0, 0.0)
+                    }
+                    stop {
+                        literal(0.2)
+                        rgb(103.0, 169.0, 207.0)
+                    }
+                    stop {
+                        literal(0.4)
+                        rgb(209.0, 229.0, 240.0)
+                    }
+                    stop {
+                        literal(0.6)
+                        rgb(253.0, 219.0, 240.0)
+                    }
+                    stop {
+                        literal(0.8)
+                        rgb(239.0, 138.0, 98.0)
+                    }
+                    stop {
+                        literal(1)
+                        rgb(178.0, 24.0, 43.0)
+                    }
+                }
+            )
+            // Increase the heatmap weight based on frequency and property magnitude
+            heatmapWeight(
+                interpolate {
+                    linear()
+                    get { literal("mag") }
+                    stop {
+                        literal(0)
+                        literal(0)
+                    }
+                    stop {
+                        literal(6)
+                        literal(1)
+                    }
+                }
+            )
+            // Increase the heatmap color weight weight by zoom level
+            // heatmap-intensity is a multiplier on top of heatmap-weight
+            heatmapIntensity(
+                interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0)
+                        literal(1)
+                    }
+                    stop {
+                        literal(9)
+                        literal(3)
+                    }
+                }
+            )
+            // Adjust the heatmap radius by zoom level
+            heatmapRadius(
+                interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0)
+                        literal(2)
+                    }
+                    stop {
+                        literal(9)
+                        literal(20)
+                    }
+                }
+            )
+            // Transition from heatmap to circle layer by zoom level
+            heatmapOpacity(
+                interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(7)
+                        literal(1)
+                    }
+                    stop {
+                        literal(9)
+                        literal(0)
+                    }
+                }
+            )
+        }
+    }
+
+    private fun createCircleLayer(): CircleLayer {
+        return circleLayer(
+            CIRCLE_LAYER_ID,
+            EARTHQUAKE_SOURCE_ID
+        ) {
+            circleRadius(
+                interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(7)
+                        interpolate {
+                            linear()
+                            get { literal("mag") }
+                            stop {
+                                literal(1)
+                                literal(1)
+                            }
+                            stop {
+                                literal(6)
+                                literal(4)
+                            }
+                        }
+                    }
+                    stop {
+                        literal(16)
+                        interpolate {
+                            linear()
+                            get { literal("mag") }
+                            stop {
+                                literal(1)
+                                literal(5)
+                            }
+                            stop {
+                                literal(6)
+                                literal(50)
+                            }
+                        }
+                    }
+                }
+            )
+            circleColor(
+                interpolate {
+                    linear()
+                    get { literal("mag") }
+                    stop {
+                        literal(1)
+                        rgba(33.0, 102.0, 172.0, 0.0)
+                    }
+                    stop {
+                        literal(2)
+                        rgb(102.0, 169.0, 207.0)
+                    }
+                    stop {
+                        literal(3)
+                        rgb(209.0, 229.0, 240.0)
+                    }
+                    stop {
+                        literal(4)
+                        rgb(253.0, 219.0, 199.0)
+                    }
+                    stop {
+                        literal(5)
+                        rgb(239.0, 138.0, 98.0)
+                    }
+                    stop {
+                        literal(6)
+                        rgb(178.0, 24.0, 43.0)
+                    }
+                }
+            )
+            circleOpacity(
+                interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(7)
+                        literal(0)
+                    }
+                    stop {
+                        literal(8)
+                        literal(1)
+                    }
+                }
+            )
+            circleStrokeColor("white")
+            circleStrokeWidth(0.1)
+        }
+    }
+
+    companion object {
+        private const val EARTHQUAKE_SOURCE_URL =
+            "https://www.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+        private const val EARTHQUAKE_SOURCE_ID = "earthquakes"
+        private const val HEATMAP_LAYER_ID = "earthquakes-heat"
+        private const val HEATMAP_LAYER_SOURCE = "earthquakes"
+        private const val CIRCLE_LAYER_ID = "earthquakes-circle"
     }
 
 }
