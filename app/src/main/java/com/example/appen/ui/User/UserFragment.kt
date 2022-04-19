b6740419ce30fd242e7f5b11387ef283dea6ade8
Chrishto <chrishto@uio.no>
2022-04-19 14:40:23 +0200

    anbefaling V1 - Ingen forskjell på verdier

--- app/src/main/java/com/example/appen/ui/User/UserFragment.kt
+++ app/src/main/java/com/example/appen/ui/User/UserFragment.kt
@@ -10,6 +10,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.appen.R
 import com.example.appen.databinding.FragmentNotificationsBinding
+import Uv
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.log
 
 class UserFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
@@ -18,6 +23,16 @@ class UserFragment : Fragment(), AdapterView.OnItemSelectedListener {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    //Score
+    var beskyttelseScore = 0
+    //aktivitet
+    lateinit var spinner: Spinner
+    //hudtype & klær
+    var seekBar1Value = 0
+    var seekBar2Value = 0
+    //lagreKnapp
+    lateinit var lagre : Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,17 +47,13 @@ class UserFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
         val textView: TextView = binding.profilTvOverspinner
         notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = "Hei, her kommer profilsiden"
+            textView.text = "Juster informasjonen så den passer deg!"
         }
-
-        val spinner: Spinner = binding.profilSpinner
-        spinner.onItemSelectedListener = this
-        val seekBar1: SeekBar = binding.profilSeekBar
-        val seekBar1Value = 0
-        val seekBar2: SeekBar = binding.profilSeekBar2
-        val seekBar2Value = 0
+        spinner = binding.profilSpinner
+        /*
         val seekBar3: SeekBar = binding.profilSeekBar3
-        val seekBar3Value = 0
+        val seekBar3Value = binding.profilSeekBar3.progress
+        */
 
         ArrayAdapter.createFromResource(
             requireContext(),
@@ -52,27 +63,10 @@ class UserFragment : Fragment(), AdapterView.OnItemSelectedListener {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
         }
-        /*
-        x+y+z
-        switchcasee/when for endelig verdi?
-        when(x){
-
-            2 -> println("This is 2")
-
-            3,4,5,6,7,8 -> println("When x is any number from 3,4,5,6,7,8")
-
-            in 9..15 -> println("When x is something from 9 to 15")
-
-            //if you want to perform some action
-            in 20..25 -> {
-                val action = "Perform some action"
-                println(action)
-            }
-
-            else -> println("When x does not belong to any of the above case")
-
+        lagre = binding.profilLagre
+        lagre.setOnClickListener{
+            kalkulerAnbefaling()
         }
-        */
         return root
     }
 
@@ -88,4 +82,94 @@ class UserFragment : Fragment(), AdapterView.OnItemSelectedListener {
     override fun onNothingSelected(p0: AdapterView<*>?) {
         TODO("Not yet implemented")
     }
+
+    fun onProgressChanged(seek: SeekBar,
+                                    progress: Int, fromUser: Boolean) {
+        // write custom code for progress is changed
+    }
+
+    fun onStartTrackingTouch(seek: SeekBar) {
+        // write custom code for progress is started
+    }
+
+    fun onStopTrackingTouch(seek: SeekBar) {
+        // write custom code for progress is stopped
+    }
+
+    fun kalkulerAnbefaling(){
+        var spinnerverdi = 0
+        val uvTime : Float = 4.0F
+        val seekBar1Value= binding.profilSeekBar.progress
+        val seekBar2Value= binding.profilSeekBar2.progress
+        var tv = binding.uvTestTv
+        when(spinner.selectedItem){
+            "Velg Aktivitet"-> spinnerverdi = 0
+            "Bytur"         -> spinnerverdi = 4
+            "Parken"        -> spinnerverdi = 1
+            "Fjelltur"      -> spinnerverdi = 2
+            "Stranda"       -> spinnerverdi = 0
+        }
+        // max score 12 - 4+4+4
+        // Ikke ulik verdi for faktorene
+        beskyttelseScore = spinnerverdi+seekBar1Value+seekBar2Value
+        Log.d("BeskyttelseScore", beskyttelseScore.toString())
+
+        // max beskyttelseScore 12
+        // when eller if (>/<)
+        when(uvTime.toInt()){
+            in 0..2     -> {
+                when(beskyttelseScore){
+                    in 0..2 -> anbefalSpf15()
+                    in 2..5 -> anbefalSpf30()
+
+                }
+            }
+            in 2..5     -> {
+                if(beskyttelseScore>=10){
+                    anbefalSpf15()
+                } else {
+                    anbefalSpf20()
+                }
+            }
+            in 5..7     -> {
+                when(beskyttelseScore){
+
+
+                }
+            }
+            in 7..10    -> {
+                when(beskyttelseScore){
+
+
+                }
+            }
+            in 10..11   -> {
+                when(beskyttelseScore){
+
+
+                }
+            }
+        }
+    }
+    fun anbefalSpf10(){
+        val tv = binding.uvTestTv
+        tv.text = "Anbefaler Spf 10"
+    }
+    fun anbefalSpf15(){
+        val tv = binding.uvTestTv
+        tv.text = "Anbefaler Spf 15"
+    }
+    fun anbefalSpf20(){
+        val tv = binding.uvTestTv
+        tv.text = "Anbefaler Spf 20"
+    }
+    fun anbefalSpf30(){
+        val tv = binding.uvTestTv
+        tv.text = "Anbefaler Spf 30"
+    }
+    fun anbefalSpf50(){
+        val tv = binding.uvTestTv
+        tv.text = "Anbefaler Spf 50"
+    }
+
 }
